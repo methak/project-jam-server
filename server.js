8a2298ed4bb9c5d751b748829f82bca93bf85de3
metha <matt.ksmp@gmail.com>
2021-10-17 21:22:49 -0700

    config server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const server = new ApolloServer({
     }
 })
 
-server.listen().then(({ url}) => {
+server.listen({ port: process.env.PORT || 4000 })
+    .then(({ url}) => {
     console.log(`🚀Server listening on ${url}🎉`);
 })
